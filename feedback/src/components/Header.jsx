d46991dde09512ec6e64026395872d570c216cea
Josh Millard <josh.millard10@gmail.com>
2022-01-05 09:03:11 -0500

    default props added to Header.jsx and propTypes imported and added to Header.jsx

--- feedback/src/components/Header.jsx
+++ feedback/src/components/Header.jsx
@@ -1,10 +1,20 @@
-function Header() {
+import PropTypes from 'prop-types'
+
+function Header({ text }) {
   return (
     <header>
       <div className="container"></div>
-      <h2>Fedback UI</h2>
+      <h2>{text}</h2>
     </header>
   )
+}
+
+Header.defaultProps = {
+  text: 'Feedback UI', 
+}
+
+Header.PropTypes = {
+  text: PropTypes.string
 }
 
 export default Header
