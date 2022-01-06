8236ccb1a0a70b78691c78ed6563dfaafd303139
Josh Millard <josh.millard10@gmail.com>
2022-01-06 18:36:52 -0500

    Button added to card component

--- feedback/src/components/Feedbackitem.jsx
+++ feedback/src/components/Feedbackitem.jsx
@@ -1,11 +1,18 @@
-import Card from './shared/Card'
+import {FaTimes} from 'react-icons/fa'
 import PropTypes from 'prop-types'
+import Card from './shared/Card'
 
 function Feedbackitem({item}) {
+  const handleClick = (id) => {
+    console.log(id)
+  }
 
   return (
     <Card reverse={true}>
-      <div className="num-display">{item.rating}</div>  
+      <div className="num-display">{item.rating}</div> 
+      <button onClick={ () => handleClick(item.id)} className="close">
+        <FaTimes  color='purple'/>  
+      </button> 
       <div className="text-display">{item.text}</div>  
     </Card>   
   )
