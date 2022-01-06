3282bbfd6d6f2955ab0064e246b88707a25f31f6
Josh Millard <josh.millard10@gmail.com>
2022-01-06 18:59:30 -0500

    FeedbackStats avg calculation implimented

--- feedback/src/components/FeedbackStats.jsx
+++ feedback/src/components/FeedbackStats.jsx
@@ -1,5 +1,23 @@
-function FeedbackStats() {
-  return <div>stats</div>
+import { ReactPropTypes } from "react"
+
+function FeedbackStats({feedback}) {
+  //calculate ratings avg
+  let average = feedback.reduce((acc, cur) => {
+    return acc + cur.rating
+  }, 0) / feedback.length
+
+  average = average.toFixed(1).replace(/[.,]0$/, '')
+
+  return (
+  <div className='feedback-stats'>
+    <h4>{feedback.length} Reviews</h4>
+    <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
+  </div>
+  )
+}
+
+FeedbackStats.propTypes = {
+  feedback: Proptypes.array.isRequired,
 }
 
 export default FeedbackStats
