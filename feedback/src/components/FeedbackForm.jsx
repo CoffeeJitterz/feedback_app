c1306abe8906ce5db248e53a18127a90b527f690
Josh Millard <josh.millard10@gmail.com>
2022-01-07 09:17:41 -0500

    btnDisabled state added to FeedbackForm and passed into Button.jsx component

--- feedback/src/components/FeedbackForm.jsx
+++ feedback/src/components/FeedbackForm.jsx
@@ -4,6 +4,8 @@ import Button from './shared/Button'
 
 function FeedbackForm() {
   const [text, setText] = useState('')
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
 
   const handleTextChange = (e) => {
     setText(e.target.value)
@@ -14,7 +16,7 @@ function FeedbackForm() {
       {/* @todo - rating select component */}
       <div className="input-group">
         <input onChange={handleTextChange} type="text" placeholder='Write a review' value={text}/>
-        <Button type="submit">Send</Button>
+        <Button type="submit" isDisabled={btnDisabled}>Send</Button>
       </div>
     </form>
   </Card>
